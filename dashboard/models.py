dc832ad19925643592a0e590751ddf13ea85a140
Niloy Islam <niloy.islam@northsouth.edu>
2023-03-05 11:07:57 +0600

    change css file & add a model

--- dashboard/models.py
+++ dashboard/models.py
@@ -26,3 +26,13 @@ class ContactForm(models.Model):
         return self.name
 
 
+
+# class Popup(models.Model):
+#     name = models.CharField(max_length=255)
+#     number = models.CharField(max_length=20)
+#     email = models.EmailField(max_length=255, blank=True, null=True)
+#     postcode = models.CharField(max_length=10, blank=True, null=True)
+
+#     def __str__(self):
+#         return self.name
+
